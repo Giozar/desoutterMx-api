d88c3fac001b78b00a4c24fd5cd5623b1b48126b
Giovanni Cortazar <manuelgiozar04@gmail.com>
2023-11-22 14:27:18 -0600

    Rutas agregadas para envio de formulario de contacto

--- src/routes/mail.routes.js
+++ src/routes/mail.routes.js
@@ -4,6 +4,8 @@ import { contact } from "../controllers/mail.controller.js";
 const mailRoutes = Router();
 
 mailRoutes.post("/contacto", contact);
+mailRoutes.post("/contacto/cotizar", contact);
+mailRoutes.post("/contacto/demo", contact);
 mailRoutes.post("/herramientas/:categoryId/:categoryName/:subcategoryId/:subcategoryName/p/:reference/:model",contact);
 mailRoutes.post("/herramientas/:categoryId/:categoryName/:subcategoryId/:subcategoryName/:subsubcategoryId/:subsubcategoryName/p/:reference/:model",contact);
 mailRoutes.post("/herramientas/:categoryId/:categoryName/:subcategoryId/:subcategoryName/:subsubcategoryId/:subsubcategoryName/:subsubsubcategoryId/:subsubsubcategoryName/p/:reference/:model",contact);
