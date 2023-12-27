c6fffb68b89dddab34111f2d458ca2426d5f426e
Giovanni Cortazar <manuelgiozar04@gmail.com>
2023-12-27 13:43:36 -0600

    Implemento de ruta para formulario de servicio

--- src/routes/mail.routes.js
+++ src/routes/mail.routes.js
@@ -6,6 +6,7 @@ const mailRoutes = Router();
 mailRoutes.post("/contacto", contact);
 mailRoutes.post("/contacto/cotizar", contact);
 mailRoutes.post("/contacto/demo", contact);
+mailRoutes.post("/contacto/servicios", contact);
 mailRoutes.post("/herramientas/:categoryId/:categoryName/:subcategoryId/:subcategoryName/p/:reference/:model",contact);
 mailRoutes.post("/herramientas/:categoryId/:categoryName/:subcategoryId/:subcategoryName/:subsubcategoryId/:subsubcategoryName/p/:reference/:model",contact);
 mailRoutes.post("/herramientas/:categoryId/:categoryName/:subcategoryId/:subcategoryName/:subsubcategoryId/:subsubcategoryName/:subsubsubcategoryId/:subsubsubcategoryName/p/:reference/:model",contact);
